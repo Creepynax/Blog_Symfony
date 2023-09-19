3bf19609ecb9ffe8d0c4bcf45652a29a337e00b9
Hywel <bonnefondhywel@gmail.com>
2023-09-19 16:50:07 +0200

    test

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -19,6 +19,9 @@ class RegistrationController extends AbstractController
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
+        $query = $entityManager->createNativeQuery('SELECT * FROM user');
+        $query->setParameter($request);
+
 
         if ($form->isSubmitted() && $form->isValid()) {
             // encode the plain password
