3859efbf71eb65824f22825466302415217a1404
Alexandre <116552630+Creepynax@users.noreply.github.com>
2023-09-21 13:55:55 +0200

    Ajout de la NAvBar + du front dans le home, login et register

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,8 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('home/home.html.twig', [
+            'controller_name' => 'Bienvenue sur le Blog HACH',
         ]);
     }
 }
