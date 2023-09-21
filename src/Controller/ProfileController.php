8f79b47851cdc3a1f71c901001a8fae6153de0c1
Hywel <bonnefondhywel@gmail.com>
2023-09-21 15:50:54 +0200

    test

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,27 +2,52 @@
 
 namespace App\Controller;
 
-use App\Repository\UserRepository;
+use App\Form\EditProfileFormType;
+use Doctrine\ORM\EntityManagerInterface; // Import the EntityManagerInterface
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
 {
-    private UserRepository $userRepository;
+    private EntityManagerInterface $entityManager; // Declare the EntityManagerInterface
 
-    public function __construct(UserRepository $userRepository)
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        $this->userRepository = $userRepository;
+        $this->entityManager = $entityManager; // Inject the EntityManagerInterface
     }
 
     #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $user = $this->getUser();
+        $editMode = $request->query->get('edit', false);
+
+        if ($editMode) {
+            $form = $this->createForm(EditProfileFormType::class, $user, [
+                'action' => $this->generateUrl('app_edit_profile'),
+                'method' => 'POST',
+            ]);
+
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $this->entityManager->flush(); // Use the injected EntityManagerInterface
+                $this->addFlash('success', 'Votre profil a été mis à jour');
+                return $this->redirectToRoute('app_profile');
+            }
+
+            return $this->render('profile/profile.html.twig', [
+                'user' => $user,
+                'editMode' => true,
+                'form' => $form->createView(),
+            ]);
+        }
 
         return $this->render('profile/profile.html.twig', [
             'user' => $user,
+            'editMode' => false,
         ]);
     }
 }
