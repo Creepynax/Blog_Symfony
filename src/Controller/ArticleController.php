ec8f41660f0dd113ab4a4e0480a85b87c41c6048
Noah <noahcharleshenry4@gmail.com>
2023-09-21 03:33:49 +0200

    Création d'article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,29 +3,60 @@
 namespace App\Controller;
 
 use App\Entity\Article;
-use App\Form\ArticleFormType;
+use App\Form\ArticleForm;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
-    #[Route('/article/create', name: 'article_create')]
-    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    /**
+     * @Route("/article/create", name="article_create")
+     */
+    public function create(Request $request, EntityManagerInterface $entityManager, UserRepository $userRepository): Response
     {
         $article = new Article();
-        $article->setUser($this->getUser());
-        $form = $this->createForm(ArticleFormType::class, $article);
+
+        // Utilisez l'ID utilisateur 7 par défaut
+        $user = $userRepository->find(7);
+        if (!$user) {
+            throw new \Exception('Utilisateur avec ID = 7 non trouvé');
+        }
+        $article->setUser($user);
+
+        $form = $this->createForm(ArticleForm::class, $article);
         $form->handleRequest($request);
 
-
         if ($form->isSubmitted() && $form->isValid()) {
+            // Gérer le fichier téléchargé
+            $file = $form['image']->getData();
+            if ($file) {
+                $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                $newFilename = uniqid() . '.' . $file->guessExtension();
+
+                try {
+                    $file->move(
+                        $this->getParameter('images_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // Gérer l'exception si quelque chose se passe mal pendant le téléchargement du fichier
+                }
+
+                $article->setImage($newFilename);
+            }
+
             $entityManager->persist($article);
             $entityManager->flush();
 
-            return $this->redirectToRoute('article_show', ['id' => $article->getId()]);
+            // Ajouter un message Flash pour indiquer le succès
+            $this->addFlash('success', 'Article créé avec succès.');
+
+            return $this->redirectToRoute('article_create');
         }
 
         return $this->render('article/article.html.twig', [
--- src/Form/ArticleForm.php
+++ src/Form/ArticleForm.php
@@ -4,51 +4,49 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
-class ArticleFormType extends AbstractType
+class ArticleForm extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title', TextType::class, [
-                'required' => true,
-            ])
-            ->add('summary', TextareaType::class, [
-                'required' => true,
-            ])
-            ->add('content', TextareaType::class, [
-                'required' => true,
-            ])
-            ->add('slug', TextType::class, [
-                'required' => true,
-            ])
+            ->add('title', TextType::class)
+            ->add('summary', TextType::class)
+            ->add('content', TextType::class)
+            ->add('slug', TextType::class)
             ->add('image', FileType::class, [
+                'label' => 'Image (fichier JPEG)',
+                'mapped' => false,
                 'required' => false,
+                'constraints' => [
+                    new \Symfony\Component\Validator\Constraints\File([
+                        'maxSize' => '2040k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Veuillez télécharger une image valide.',
+                    ])
+                ],
             ])
-            ->add('submit', SubmitType::class, [
-                'label' => 'Créer',
-                'attr' => ['class' => 'btn btn-success'],
+            ->add('date', DateTimeType::class, [
+                'widget' => 'single_text'
             ])
-            ->add('date', DateType::class, [
-                'required' => true,
-                'widget' => 'single_text',
+            ->add('save', SubmitType::class, [
+                'label' => 'Créer un article'
             ]);
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => Article::class,
         ]);
     }
 }
-
-
